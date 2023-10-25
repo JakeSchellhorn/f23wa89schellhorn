df0388ea17dd10d7629167d6a747c6912bab69a4
Jake Schellhorn <j.schellhorn.02@gmail.com>
2023-10-25 13:24:06 -0500

    fixing endpoint

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(render("/mydata"));
+app.use(render("./routes/mydata"));
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
